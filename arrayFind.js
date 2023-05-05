385f89acf7c7ab9500fa13c10c90082d2bc7d356
yashborda7048 <yashborda123@gmail.com>
2023-05-05 17:02:58 +0530

    Array in object value find first value

---
+++ arrayFind.js
@@ -0,0 +1,29 @@
+// program to find the Array in object value find first value
+
+// take input from the user
+const inventory = [
+  {
+    name: "apples",
+    quantity: 2,
+  },
+  {
+    name: "bananas",
+    quantity: 0,
+  },
+  {
+    name: "cherries",
+    quantity: 5,
+  },
+  {
+    name: "cherries",
+    quantity: 8,
+  },
+  {
+    name: "cherries",
+    quantity: 9,
+  },
+];
+
+const result = inventory.find(({ name }) => name === "cherries");
+
+console.log(result); // { name: 'cherries', quantity: 5 }
