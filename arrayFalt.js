082971b363058973dee9d05b9328c19c8d825357
yashborda7048 <yashborda123@gmail.com>
2023-05-05 17:10:35 +0530

    Array in arary and maltipal array push to sigal array

---
+++ arrayFalt.js
@@ -0,0 +1,18 @@
+// program to find the Array in arary and maltipal array push to sigal array
+
+// take input from the user
+const arr1 = [1, 2, [3, 4]];
+arr1.flat();
+// [1, 2, 3, 4]
+
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+arr2.flat();
+// [1, 2, 3, 4, [5, 6]]
+
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+arr3.flat(2);
+// [1, 2, 3, 4, 5, 6]
+
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+arr4.flat(Infinity);
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
