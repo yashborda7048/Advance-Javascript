1d052cccfdcda99ebcc11f016a437434d2d081ad
yashborda7048 <yashborda123@gmail.com>
2023-05-05 15:59:33 +0530

    program to find the Array in object value

---
+++ arrayFiler.js
@@ -0,0 +1,50 @@
+// program to find the Array in object value
+
+// take input from the user
+
+var emp = [
+  {
+    name: "yash",
+    age: 21,
+    salary: 20000,
+  },
+  {
+    name: "kano xyz",
+    age: 19,
+    salary: 10000,
+  },
+  {
+    name: "kano abc",
+    age: 18,
+    salary: 10000,
+  },
+  {
+    name: "jenish",
+    age: 20,
+    salary: 12000,
+  },
+  {
+    name: "abhishek",
+    age: 20,
+    salary: 12000,
+  },
+];
+
+var input = prompt(); // kan
+
+var b = a.filter((val) => val.name.includes(input.toLowerCase));
+
+// output
+console.log(b);
+// [
+//   {
+//     name: "kano xyz",
+//     age: 19,
+//     salary: 10000,
+//   },
+//   {
+//     name: "kano abc",
+//     age: 18, 
+//     salary: 10000,
+//   },
+// ];
